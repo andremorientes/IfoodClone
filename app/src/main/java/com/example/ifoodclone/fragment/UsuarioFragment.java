e0f6539115395570da232d4d54fad1e9064d705f
André Morientes <59062594+andremorientes@users.noreply.github.com>
2022-06-01 12:00:12 +0200

    Criei a tela de Login geral, cadstro para usuario ,e para empresa, usei a TabLayout , fiz a validacao de dados e salvar noFireBase

--- app/src/main/java/com/example/ifoodclone/fragment/UsuarioFragment.java
+++ app/src/main/java/com/example/ifoodclone/fragment/UsuarioFragment.java
@@ -1,22 +1,121 @@
 package com.example.ifoodclone.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 import com.example.ifoodclone.R;
+import com.example.ifoodclone.helper.FirebaseHelper;
+import com.example.ifoodclone.model.Login;
+import com.example.ifoodclone.model.Usuario;
 
 public class UsuarioFragment extends Fragment {
+    private EditText edt_email;
+    private EditText edt_senha;
+    private Button btn_criar_conta;
+    private ProgressBar progressBar;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_usuario, container, false);
+        View view = inflater.inflate(R.layout.fragment_usuario, container, false);
+
+        iniciaComponentes(view);
+        configCliques();
+        return view;
+    }
+
+    private void configCliques() {
+        btn_criar_conta.setOnClickListener(v -> validaDados());
+
+    }
+
+    private void validaDados() {
+
+        String email = edt_email.getText().toString();
+        String senha = edt_senha.getText().toString();
+
+        if (!email.isEmpty()){
+            if (!senha.isEmpty()){
+
+                progressBar.setVisibility(View.VISIBLE);
+
+                Usuario usuario= new Usuario();
+                usuario.setEmail(email);
+                usuario.setSenha(senha);
+
+                criarConta(usuario);
+
+            }else {
+                edt_senha.requestFocus();
+                edt_senha.setError("Informe sua Senha");
+            }
+
+        }else{
+            edt_email.requestFocus();
+            edt_email.setError("Informe o seu e-mail");
+        }
+
+    }
+
+    private void criarConta(Usuario usuario){
+        FirebaseHelper.getAuth().createUserWithEmailAndPassword(
+                usuario.getEmail(), usuario.getSenha()
+        ).addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                ocultarTeclado();
+
+                String id= task.getResult().getUser().getUid();
+                usuario.setId(id);
+                usuario.salvar();
+
+                Login login= new Login(id,"U",false);
+                login.salvar();
+
+                //getActivity().finish();
+
+
+            }else{
+                progressBar.setVisibility(View.GONE);
+                erroAutenticacao(FirebaseHelper.validaErros(task.getException().getMessage()));
+            }
+        });
+    }
+
+    private void erroAutenticacao(String msg){
+        AlertDialog.Builder builder= new AlertDialog.Builder(requireActivity());
+        builder.setTitle("Atençao");
+        builder.setMessage(msg);
+        builder.setPositiveButton("OK", ((dialog, which) -> {
+            dialog.dismiss();
+        }));
+        AlertDialog dialog= builder.create();
+        dialog.show();
+    }
+
+    private void iniciaComponentes(View view) {
+        edt_email = view.findViewById(R.id.edt_email);
+        edt_senha = view.findViewById(R.id.edt_senha);
+        btn_criar_conta = view.findViewById(R.id.btn_criar_conta);
+        progressBar = view.findViewById(R.id.progressBar);
+
+    }
+
+    private  void ocultarTeclado(){
+        ((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromWindow(
+                btn_criar_conta.getWindowToken(),0
+        );
     }
 }
