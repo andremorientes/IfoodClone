22adf811b88e80103a0280ed1676427aad4b99f6
André Morientes <59062594+andremorientes@users.noreply.github.com>
2022-06-07 10:26:57 +0200

    Criando Fragment de Perfil de Usuario .. e instacionando no UsuarioHomeActivity

--- app/src/main/java/com/example/ifoodclone/empresa/EmpresaHomeActivity.java
+++ app/src/main/java/com/example/ifoodclone/empresa/EmpresaHomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.ifoodclone.empresa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.ifoodclone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class EmpresaHomeActivity extends AppCompatActivity {
 
@@ -12,5 +16,9 @@ public class EmpresaHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_empresa_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController= Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
--- app/src/main/java/com/example/ifoodclone/usuario/UsuarioHomeActivity.java
+++ app/src/main/java/com/example/ifoodclone/usuario/UsuarioHomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.ifoodclone.usuario;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
 import com.example.ifoodclone.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class UsuarioHomeActivity extends AppCompatActivity {
 
@@ -12,5 +16,9 @@ public class UsuarioHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usuario_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController= Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
     }
 }
