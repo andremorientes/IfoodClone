baf1ca22d2daf0c18e506b8f8145d5ffb6ec6517
André Morientes <59062594+andremorientes@users.noreply.github.com>
2022-08-09 14:02:26 +0200

    PEDIDOS NO CARRINHO , EXIBINDO NA PAGINA HOME DO USUARIO

--- app/src/main/java/com/example/ifoodclone/fragment/usuario/UsuarioHomeFragment.java
+++ app/src/main/java/com/example/ifoodclone/fragment/usuario/UsuarioHomeFragment.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,15 +12,20 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ifoodclone.DAO.EmpresaDAO;
+import com.example.ifoodclone.DAO.ItemPedidoDAO;
 import com.example.ifoodclone.R;
 import com.example.ifoodclone.adapter.EmpresasAdapter;
 import com.example.ifoodclone.empresa.EmpresaCardapioActivity;
 import com.example.ifoodclone.helper.FirebaseHelper;
+import com.example.ifoodclone.helper.GetMask;
 import com.example.ifoodclone.model.Empresa;
+import com.example.ifoodclone.usuario.CarrinhoActivity;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -40,7 +46,13 @@ public class UsuarioHomeFragment extends Fragment implements EmpresasAdapter.OnC
     private ProgressBar progressBar;
     private TextView text_info;
 
+    private TextView textQtdItemSacola;
+    private TextView textTotalCarrinho;
+    private ConstraintLayout ll_sacola;
 
+
+    private ItemPedidoDAO itemPedidoDAO;
+    private EmpresaDAO empresaDAO;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -48,10 +60,25 @@ public class UsuarioHomeFragment extends Fragment implements EmpresasAdapter.OnC
         // Inflate the layout for this fragment
         View view= inflater.inflate(R.layout.fragment_usuario_home, container, false);
 
+        itemPedidoDAO= new ItemPedidoDAO(getContext());
+        empresaDAO= new EmpresaDAO(getContext());
+
         iniciaComponentes(view);
+        configCliques();
         configRv();
         recuperaEmpresas();
         return  view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        configSacola();
+    }
+
+    private void configCliques(){
+        ll_sacola.setOnClickListener(v ->
+                startActivity(new Intent(getActivity(), CarrinhoActivity.class)));
     }
 
     private void configRv(){
@@ -89,11 +116,30 @@ public class UsuarioHomeFragment extends Fragment implements EmpresasAdapter.OnC
         });
     }
 
+    private void configSacola(){
+        if (!itemPedidoDAO.getList().isEmpty()){
+
+            double totalPedido= itemPedidoDAO.getTotal() + empresaDAO.getEmpresa().getTaxaEntrega();
+            ll_sacola.setVisibility(View.VISIBLE);
+            textQtdItemSacola.setText(String.valueOf(itemPedidoDAO.getList().size()));
+            textTotalCarrinho.setText(getString(R.string.text_valor, GetMask.getValor(totalPedido)));
+
+        }else{
+            ll_sacola.setVisibility(View.GONE);
+            textQtdItemSacola.setText("");
+            textTotalCarrinho.setText("");
+        }
+    }
+
     private void iniciaComponentes(View  view){
 
         rv_empresas= view.findViewById(R.id.rv_empresas);
         progressBar= view.findViewById(R.id.progressBar);
         text_info= view.findViewById(R.id.text_info);
+
+        textQtdItemSacola= view.findViewById(R.id.textQtdItemSacola);
+        textTotalCarrinho= view.findViewById(R.id.textTotalCarrinho);
+        ll_sacola= view.findViewById(R.id.ll_sacola);
     }
 
     @Override
